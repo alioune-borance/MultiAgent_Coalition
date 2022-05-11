afd68eb07c59db187d0e750fbb331f2eef66658b
alioune-borance <borancembaye98@gmail.com>
2022-05-11 16:00:29 +0000

    initial commit

---
+++ src/AgentA.java
@@ -0,0 +1,94 @@
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class AgentA extends Agent{
+	public void setup() {
+		System.out.println("Agent A : " + getAID().getName());
+		
+		  FSMBehaviour agentA_beh= new FSMBehaviour();
+		  
+		  agentA_beh.registerFirstState(new envoiProposition(), "envoiPropositionA");
+		  agentA_beh.registerState(new attendreAcceptation(), "attendreAcceptationA");
+		  agentA_beh.registerState(new confirmerProposition(), "confirmerPropositionA");
+		  agentA_beh.registerLastState(new fin(), "fin");
+
+		  agentA_beh.registerDefaultTransition("envoiPropositionA", "attendreAcceptationA");
+		  agentA_beh.registerTransition("attendreAcceptationA", "confirmerPropositionA",0);
+		  agentA_beh.registerTransition("confirmerPropositionA", "fin", 1);
+		  
+		  addBehaviour(agentA_beh);  
+		
+	}
+	
+	protected void takeDown() {
+		// Printout a dismissal message
+		System.out.println("agent "+getAID().getName()+" terminating.");
+		}
+	
+	
+	
+	
+	private class envoiProposition extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Envoi de proposition A /A ]");
+			ACLMessage msg = new ACLMessage(ACLMessage.PROPOSE);
+			msg.addReceiver(new AID("AgentB",AID.ISLOCALNAME));
+			msg.setContent("Proposition A1");
+			send(msg);
+		}
+		
+	}
+	
+	
+	private class attendreAcceptation extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Attendre acceptation proposition A /A ]");
+			MessageTemplate modele = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msgRecu = receive(modele);
+			if (msgRecu != null) {
+				String n = msgRecu.getContent();
+				AID id = msgRecu.getSender();
+				System.out.println("Acceptation "+ n +" reçu par " + id.getLocalName());
+				}
+			else {
+				block();
+			}
+		}
+		
+	}
+	
+	private class confirmerProposition extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Confirmer proposition A /A ]");
+			ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
+			msg.addReceiver(new AID("AgentB",AID.ISLOCALNAME));
+			msg.setContent("Confirmation A1");
+			send(msg);
+		}
+		
+		public int onEnd() {
+			myAgent.doDelete();
+		    return super.onEnd();
+		}
+	}
+	
+	private class fin extends OneShotBehaviour {
+		public void action() {
+			MessageTemplate modele = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+			ACLMessage msgRecu = receive(modele);
+			if (msgRecu != null) {
+			System.out.println("Fin de l'agent " + myAgent.getLocalName());
+			myAgent.doDelete();
+		
+		}
+			else {
+				block();
+			}
+			
+
+	}
+	}
+}
---
+++ src/AgentB.java
@@ -0,0 +1,115 @@
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class AgentB extends Agent{
+	public void setup() {
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Agent B : " + getAID().getName());
+		
+		  FSMBehaviour agentB_beh= new FSMBehaviour();
+		  
+		  agentB_beh.registerFirstState(new attendreProposition(), "attendrePropositionA");
+		  agentB_beh.registerState(new accepterPropostion(), "acceptationPropositionA");
+		  agentB_beh.registerState(new attendreConfirmation(), "attendreConfirmationA");
+		  agentB_beh.registerState(new confirmerProposition(), "confirmerPropositionA");
+		  agentB_beh.registerState(new fin(), "fin");
+
+		  agentB_beh.registerDefaultTransition("attendrePropositionA", "acceptationPropositionA");
+		  agentB_beh.registerTransition("acceptationPropositionA", "attendreConfirmationA",0);
+		  agentB_beh.registerTransition("attendreConfirmationA", "confirmerPropositionA",0);
+		  agentB_beh.registerTransition("confirmerPropositionA", "fin", 1);
+		  
+		  addBehaviour(agentB_beh);
+		  
+	}
+	
+	protected void takeDown() {
+		// Printout a dismissal message
+		System.out.println("agent "+getAID().getName()+" terminating.");
+		}
+	
+	private class attendreProposition extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Attente de la proposition A /B ]");
+			MessageTemplate modele = MessageTemplate.MatchPerformative(ACLMessage.PROPOSE);
+			ACLMessage msgRecu = receive(modele);
+			if (msgRecu != null) {
+				String n = msgRecu.getContent();
+				AID id = msgRecu.getSender();
+				System.out.println("Propostion "+ n +" reçu par " + id.getLocalName());
+				}
+			else {
+				block();
+			}
+		}
+		
+	}
+	
+	private class accepterPropostion extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Accepter proposition A /B ]");
+			ACLMessage msg = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+			msg.addReceiver(new AID("AgentA",AID.ISLOCALNAME));
+			msg.setContent("Acceptation A1");
+			send(msg);
+		}
+		
+	}
+	
+	
+	private class attendreConfirmation extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Attendre confirmation proposition A /B ]");
+			MessageTemplate modele = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+			ACLMessage msgRecu = receive(modele);
+			if (msgRecu != null) {
+				String n = msgRecu.getContent();
+				AID id = msgRecu.getSender();
+				System.out.println("[ Confirmation "+ n +" reçu par " + id.getLocalName() + "]");
+				}
+			else {
+				block();
+			}
+		}
+		
+	}
+	
+	private class confirmerProposition extends OneShotBehaviour {
+		public void action() {
+			System.out.println("[ Confirmer proposition A /B ]");
+			ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
+			msg.addReceiver(new AID("AgentA",AID.ISLOCALNAME));
+			msg.setContent("Confirmation A1");
+			send(msg);
+		}
+		
+		public int onEnd() {
+			myAgent.doDelete();
+		    return super.onEnd();
+		}
+		
+	}
+	
+	private class fin extends OneShotBehaviour {
+		public void action() {
+			MessageTemplate modele = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+			ACLMessage msgRecu = receive(modele);
+			if (msgRecu != null) {
+			System.out.println("Fin de l'agent " + myAgent.getLocalName());
+			myAgent.doDelete();
+			}
+			else {
+				block();
+			}
+		
+		}
+	}
+}
